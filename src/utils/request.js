25105fdbc81a6224f9f239f547ce5726190b1c54
Ethan <757819951@qq.com>
2025-03-24 10:31:27 +0800

    procurement p

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import { useUserStore } from '@/stores/user'
 import router from '@/router'
 
 const service = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '',
   timeout: 5000
 })
 
